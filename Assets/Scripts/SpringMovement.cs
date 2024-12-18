61611535341c8664a351c5abee934774d83bfa2e
Lantzel0t <lantzel0t@users.noreply.github.com>
2024-12-18 17:53:00 -0600

    Plunger!

--- Assets/Scripts/SpringMovement.cs
+++ Assets/Scripts/SpringMovement.cs
@@ -4,14 +4,19 @@ public class MoveObjectOnSpace : MonoBehaviour
 {
     public float moveSpeed = 1f; // Speed at which the object moves along the X-axis
     
-    
-    private Vector3 resetPosition; // Store the initial position of the object
-    private Vector3 initialPosition;
+    public float maxPullback = 0f;
+    private Vector3 maxPullbackPos; 
+    private Vector3 initialPosition; // Store the initial position of the object
+    private Rigidbody rigidbody;
 
     void Start()
     {
-        // Store the initial position when the script starts
-        resetPosition = new Vector3(0.433f, 0.137f , 2.256f);
+        // Store the initial position and RB when the script starts
+        initialPosition = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+        // Figure out how far back we can pull
+        maxPullbackPos = initialPosition;
+        maxPullbackPos.x += maxPullback;
     }
 
     void Update()
@@ -20,12 +25,12 @@ public class MoveObjectOnSpace : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             // Move the object along the x-axis
-            transform.position = new Vector3(initialPosition.x + moveSpeed * Time.time, transform.position.y, transform.position.z);
+            rigidbody.MovePosition(Vector3.Lerp(transform.position, maxPullbackPos, moveSpeed));
         }
         else
         {
             // Reset the object's position to the initial one when spacebar is not held
-            transform.position = resetPosition;
+            rigidbody.MovePosition(Vector3.Lerp(transform.position, initialPosition, 0.5f));
         }
     }
 }
